52366bad9d76fb6ae129bce6c4c30a809c634548
Jordan Phillips <jordan.m.phillips23@gmail.com>
2018-10-02 08:07:19 -0400

    initial

---
+++ src/ProblemSet3.java
@@ -0,0 +1,65 @@
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 3 will be more extensive than the first two. You've been warned. Practice makes perfect,
+ * and we'll get a lot of practice with conditional and iterative control structures.
+ * 
+ * A few new concepts are being introduced with this assignment. Last time, we saw methods (other than
+ * the @main method) for the first time. Now, we're seeing parameters, which are the variables inside
+ * the parentheses in our methods. We'll cover these in more detail later. For now, you just need to
+ * understand the basics.
+ * 
+ * A parameter is data that is provided to the method. It helps the method do its job. It's passed in
+ * through the parentheses, and you can reference these variables by name as if they're your own.
+ */
+
+public class ProblemSet3 {
+	
+	/**
+	 * Make sure you're testing your code by calling your methods from the main method!
+	 */
+	
+	public static void main(String[] args) {
+			
+	}
+		
+	public void dateFashion(int you, int date) {
+		
+	}
+	
+	public void fizzString(String str) {
+		
+	}
+		
+	public void squirrelPlay(int temp, boolean isSummer) {
+		
+	}
+	
+	public void fizzStringAgain(int n) {
+		
+	}
+	
+	public void makeBricks(int small, int big, int goal) {
+		
+	}
+	
+	public void loneSum(int a, int b, int c) {
+		
+	}
+	
+	public void luckySum(int a, int b, int c) {
+		
+	}
+	
+	public void factorialWithFor(int n) {
+		
+	}
+	
+	public void factorialWithWhile(int n) {
+		
+	}
+	
+	public void isPrime(int n) {
+		
+	}
+}
